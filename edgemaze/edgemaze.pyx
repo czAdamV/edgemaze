d4cc7a12a3cc131d7e978b26538001b89bfc4ee0
Adam Volek <volek@adamv.cz>
2019-11-18 09:45:53 +0100

    Just inline everything
    Seriously though, why are function calls so expensive in Cython?

--- edgemaze/edgemaze.pyx
+++ edgemaze/edgemaze.pyx
@@ -1,3 +1,4 @@
+# cython: boundscheck=False, wraparound=False
 from libcpp.deque cimport deque
 from libcpp.pair cimport pair
 from operator import add
@@ -54,53 +55,14 @@ def maze_tarets(maze):
     return zip(*numpy.where(maze & 0b001))
 
 
-# This feels kinda ugly but I couldn't think of a more elegant way to do this.
-def neighbours(maze, tile):
-    ret = []
-
-    # Can I go to the left?
-    if tile[1] > 0 and not maze[tile] & 0b010:
-        ret.append(((tile[0], tile[1] - 1), b'>'))
-
-    # Can I go up?
-    if tile[0] > 0 and not maze[tile] & 0b100:
-        ret.append(((tile[0] - 1, tile[1]), b'v'))
-
-    # Can I go to the right?
-    if tile[1] + 1 < maze.shape[1] and not maze[tile[0]][tile[1]+1] & 0b010:
-        ret.append(((tile[0] , tile[1] + 1), b'<'))
-
-    # Can I go down?
-    if tile[0] + 1 < maze.shape[0] and not maze[tile[0]+1][tile[1]] & 0b100:
-        ret.append(((tile[0] + 1, tile[1]), b'^'))
-
-    return ret
-
-
-cdef step_flood(
-    numpy.ndarray[numpy.int8_t, ndim=2] maze,
-    numpy.ndarray[numpy.int64_t, ndim=2] distances,
-    numpy.ndarray[numpy.int8_t, ndim=2] directions,
-    deque[pair[int, int]] &queue
-):
-    current = queue.front()
-    queue.pop_front()
-
-    for next, direction in neighbours(maze, current):
-        if distances[next] != -1:
-            continue
-
-        distances[next] = distances[current] + 1
-        directions[next] = direction
-
-        queue.push_back(next)
-
-
 def analyze(maze):
-    cdef deque[pair[int, int]] flood_queue
-    cdef numpy.ndarray[numpy.int64_t, ndim=2] distances
-    cdef numpy.ndarray[numpy.int8_t, ndim=2] directions
-    cdef numpy.ndarray[numpy.int8_t, ndim=2] maze_int
+    cdef int                                    count, x, y
+    cdef pair[int, int]                         current
+    cdef deque[pair[int, int]]                  flood_queue
+    cdef numpy.ndarray[char, ndim=2]            directions
+    cdef numpy.ndarray[numpy.int8_t, ndim=2]    maze_int
+    cdef numpy.ndarray[numpy.int64_t, ndim=2]   distances
+    cdef pair[int[2], char]                     nexts[4]
 
     if maze.ndim != 2:
         raise TypeError(f'Maze dimension must be two, {maze.ndim} given.')
@@ -119,6 +81,51 @@ def analyze(maze):
         flood_queue.push_back(target)
 
     while not flood_queue.empty():
-        step_flood(maze_int, distances, directions, flood_queue)
+        current = flood_queue.front()
+        flood_queue.pop_front()
+
+        count = 0
+
+        x = current.first
+        y = current.second
+
+        # Can I go to the left?
+        if y > 0 and not maze_int[x, y] & 0b010:
+            nexts[count].first[0] = x
+            nexts[count].first[1] = y - 1
+            nexts[count].second = b'>'
+            count += 1
+
+        # Can I go up?
+        if x > 0 and not maze_int[x, y] & 0b100:
+            nexts[count].first[0] = x  - 1
+            nexts[count].first[1] = y
+            nexts[count].second = b'v'
+            count += 1
+
+        # Can I go to the right?
+        if y + 1 < maze_int.shape[1] and not maze_int[x, y+1] & 0b010:
+            nexts[count].first[0] = x
+            nexts[count].first[1] = y + 1
+            nexts[count].second = b'<'
+            count += 1
+
+        # Can I go down?
+        if x + 1 < maze_int.shape[0] and not maze_int[x+1, y] & 0b100:
+            nexts[count].first[0] = x  + 1
+            nexts[count].first[1] = y
+            nexts[count].second = b'^'
+            count += 1
+
+        for i in range(count):
+            x = nexts[i].first[0]
+            y = nexts[i].first[1]
+            if distances[x, y] != -1:
+                continue
+
+            distances[x, y] = distances[current.first, current.second] + 1
+            directions[x, y] = nexts[i].second
+
+            flood_queue.push_back(pair[int, int](x, y))
 
     return Solution(distances, directions, numpy.all(distances != -1))
